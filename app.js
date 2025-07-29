04116acfe396da954588a26f85c9dd1d42601c4e
Matheus <matheusp.sampaio@gmail.com>
2025-07-28 22:21:10 -0300

    Implementar a função que exibe os amigos na tela

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = [];
+
+function atualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i];
+
+        let novoElemento = document.createElement('li');
+        novoElemento.textContent = amigo;
+        
+        lista.appendChild(novoElemento);   
+    }
+}
